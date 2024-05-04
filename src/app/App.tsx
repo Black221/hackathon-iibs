b5296a4807e14e406cb6a400f024faf6c6627726
LHackSRT <bambabar02@gmail.com>
2024-05-04 13:38:56 +0000

    standard file init

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,8 @@
 import { Route, Routes} from 'react-router-dom'
 import Layout from "./Layout"
 
+import AdminPage from "./admin/Page";
+
 function App() {
 
 
@@ -12,9 +14,10 @@ function App() {
 					<div />
 				} />
 
-				<Route path="/marketplace" element={<div> </div> } />
-				<Route path="/profile" element={<div> Profile </div> } />
-				<Route path="/panier" element={<div> Panier </div> } />
+				<Route path="/admin" element={ <AdminPage />} />
+				<Route path="/student" element={<div> </div> } />
+				<Route path="/teacher" element={<div> </div> } />
+				
 
 				<Route path="*" element={<div>404</div>} />
 
---
+++ src/app/admin/Page.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router";
+import Layout from "./Layout";
+
+
+
+export default function Page () {
+
+    return(<>
+        <Routes>
+            <Route element={<Layout />}>
+                <Route path="/" element={<div > Bouna</div>} />
+            </Route>
+        </Routes>
+    </>)
+}
