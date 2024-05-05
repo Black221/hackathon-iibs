5ece2e931a64a5db40293c52365e9fc21f95d8ab
Mahomed Cissokho <mahomedcissokho@esp.sn>
2024-05-05 14:01:19 +0000

    A lot changes on Admin UI

--- src/app/admin/notification/ListNotification.tsx
+++ src/app/admin/notification/ListNotification.tsx
@@ -1,9 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Table } from "../../../components/Table";
+import { COLUMNS_NOTIFICATIONS, LISTNOTIFICATIONS } from "../../../data/dummy";
 
 
 
-export default function Notification() {
+export default function ListNotification() {
 
-    return(<>
-    
+    const ACTIONS = [
+        {
+            element: <button>Details</button>,
+            ref: "name",
+            onClick: (data: string | number) => {
+                setQuery(prev => prev + "&" + data);
+            },
+        },
+    ];
+
+
+    return (<>
+
+        <div className="flex gap-5 flex-col">
+            <Table label="Liste des Notifications" columns={COLUMNS_NOTIFICATIONS} data={ LISTNOTIFICATIONS} actions={ACTIONS}  />
+        </div>
     </>)
 }
+
+
+function setQuery(_arg0: (prev: any) => string) {
+    throw new Error("Function not implemented.");
+}
+
